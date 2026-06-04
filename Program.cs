2d775c00e80173456c6a8d0fa1a4d15f6d0dc750
Shrutika-Pineapple <web.designer@pbsworld.com>
2026-06-04 16:01:55 +0530

    Revert Called

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Zinal Shah From C# language!");
+﻿
+Console.WriteLine("Hello, Zinal Shah From C# language!ttt");
 
