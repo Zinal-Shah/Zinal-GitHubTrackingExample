3af028cda64b450b7c2e2a3bd4412f73fddb3269
Zinal-Shah <pbs.zinal.shah@gmail.com>
2026-06-04 15:24:47 +0530

    Remove wrong Name.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Zinal Shah From C# language! This is From Zianl");
+Console.WriteLine("Hello, Zinal Shah From C# language!");
